44588062569e6b2b8f99b5e26cd430b1a35aace3
Anurag Kashyap <swiftycodes@gmail.com>
2023-05-03 19:47:15 +0530

    Draw direction between routes source and destination.

--- UberClone/Core/Home/Views/UberMapViewRepresentable.swift
+++ UberClone/Core/Home/Views/UberMapViewRepresentable.swift
@@ -21,12 +21,12 @@ struct UberMapViewRepresentable : UIViewRepresentable {
     
     let mapView = MKMapView()
     let locationManager = LocationManager()
-
+    
     func makeUIView(context: Context) -> some UIView {
         // Do the initial Set up here
         //context.coordinator --> is the MapCoodinator here or the middleman // the Mapview Delegate things / Functionality Level things
         mapView.delegate = context.coordinator
-        mapView.isRotateEnabled = true
+        mapView.isRotateEnabled = false
         mapView.showsUserLocation = true
         mapView.userTrackingMode = .follow
         return mapView
@@ -35,8 +35,11 @@ struct UberMapViewRepresentable : UIViewRepresentable {
     func updateUIView(_ uiView: UIViewType, context: Context) {
         // Update the Views or Mapview in this case here
         //Like draw lines and etc.
-        if let selectedLocation = locationSearchViewModel.selectedLocation {
-            print("DEBUG : selected location is \(selectedLocation)")
+        
+        if let coordinateOfLocationSelected = locationSearchViewModel.selectedLocationCordinate {
+            //print("DEBUG : Location cordinates \(selectedLocation)")
+            context.coordinator.addAndSelectCoordinates(withCoordinates: coordinateOfLocationSelected)
+            context.coordinator.configurePolyLine(withDestinationCoordinate: coordinateOfLocationSelected)
         }
     }
     
@@ -58,21 +61,79 @@ extension UberMapViewRepresentable {
     
     class MapCordinator : NSObject, MKMapViewDelegate {
         
+        //MARK: - Properties
+        
         let parent : UberMapViewRepresentable
+        var userLocationCoordinate : CLLocationCoordinate2D?
+        
+        //MARK: - Life Cyclyes
         
         init(parent: UberMapViewRepresentable) {
             self.parent = parent
             super.init()
         }
         
+        //MARK: - Map View Delegates
+        
         //Here we set the location on map
         func mapView(_ mapView: MKMapView, didUpdate userLocation: MKUserLocation) {
+            self.userLocationCoordinate = userLocation.coordinate
             //span - is the amount of zoom we want on that cordinate.
             let regionArea = MKCoordinateRegion(
                 center: CLLocationCoordinate2D(latitude: userLocation.coordinate.latitude, longitude: userLocation.coordinate.longitude),
-                span: MKCoordinateSpan(latitudeDelta: 0.005, longitudeDelta: 0.005))
+                span: MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05))
+            parent.mapView.setRegion(regionArea, animated: true)
+        }
+        
+        //Display the PolyLine on Map because of this.
+        func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+            let polyLine = MKPolylineRenderer(overlay: overlay)
+            polyLine.strokeColor = .blue
+            polyLine.lineWidth = 4
+            return polyLine
+        }
+        
+        //MARK: - Helpers
+        
+        func addAndSelectCoordinates(withCoordinates coordinates : CLLocationCoordinate2D) {
+            //remove all added anotations
+            parent.mapView.removeAnnotations(parent.mapView.annotations)
+            //add anotation
+            let annotation = MKPointAnnotation()
+            annotation.coordinate = coordinates
+            self.parent.mapView.addAnnotation(annotation)
+            self.parent.mapView.selectAnnotation(annotation, animated: true)
+            self.parent.mapView.showAnnotations(parent.mapView.annotations, animated: true)
+        }
+        
+        func configurePolyLine(withDestinationCoordinate destinationCoordinate : CLLocationCoordinate2D) {
             
-                parent.mapView.setRegion(regionArea, animated: true)
+            guard let userCoordinate = self.userLocationCoordinate  else {return}
+            
+            getDestinationRoute(from: userCoordinate, to: destinationCoordinate) { route in
+                //Creates a Poly Line here
+                self.parent.mapView.addOverlay(route.polyline)
+            }
+        }
+        
+        func getDestinationRoute(from userLocation : CLLocationCoordinate2D , to destination : CLLocationCoordinate2D, completion: @escaping (MKRoute) -> Void) {
+            
+            let userPlaceMark = MKPlacemark(coordinate: userLocation)
+            let destinationPlaceMark = MKPlacemark(coordinate: destination)
+            let request = MKDirections.Request()
+            request.source = MKMapItem(placemark: userPlaceMark)
+            request.destination = MKMapItem(placemark: destinationPlaceMark)
+            
+            let direction = MKDirections(request: request)
+            direction.calculate { response, error in
+                
+                if let err = error {
+                    print("DEBUG : Fail with error \(err.localizedDescription)")
+                }
+                
+                guard let route = response?.routes.first else {return}
+                completion(route)
+            }
         }
     }
 }
--- UberClone/Core/Location Search/View Model/LocationSearchViewModel.swift
+++ UberClone/Core/Location Search/View Model/LocationSearchViewModel.swift
@@ -13,7 +13,7 @@ class LocationSearchViewModel : NSObject , ObservableObject {
     // MARK: - Properties
     
     @Published var results = [MKLocalSearchCompletion]()
-    @Published var selectedLocation : String?
+    @Published var selectedLocationCordinate : CLLocationCoordinate2D?
     
     private let searchCompleter = MKLocalSearchCompleter()
     var queryFragment : String = ""{
@@ -29,8 +29,24 @@ class LocationSearchViewModel : NSObject , ObservableObject {
         searchCompleter.queryFragment = queryFragment
     }
     
-    func selectedLocation(_ location : String) {
-        self.selectedLocation = location
+    func selectedLocation(_ localSearch : MKLocalSearchCompletion) {
+        //Here we will use the title and sub title to make the cordinates -- Geo Reverse to Cordinates
+        locationSearch(forLoaclSearchCompletion: localSearch) { response, error in
+            if let err = error {
+                print("DEBUG : Location search fail with error \(err.localizedDescription)")
+                return
+            }
+            guard let item = response?.mapItems.first else {return}
+            let cordinate = item.placemark.coordinate
+            self.selectedLocationCordinate = cordinate
+        }
+    }
+    
+    func locationSearch(forLoaclSearchCompletion localSearch : MKLocalSearchCompletion, completion : @escaping(MKLocalSearch.CompletionHandler)) {
+        let searchRequest = MKLocalSearch.Request()
+        searchRequest.naturalLanguageQuery = localSearch.title.appending(localSearch.subtitle)
+        let search = MKLocalSearch(request: searchRequest)
+        search.start(completionHandler: completion)
     }
 }
 
